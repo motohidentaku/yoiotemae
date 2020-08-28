18856ef0c115b0bbecf1c13d0ac7fad03d87197c
pooseijin <pooseijin@motohidentaku.com>
2020-08-29 08:05:22 +0900

    edit sg90

--- yoiotemae/lib/sg90.py
+++ yoiotemae/lib/sg90.py
@@ -9,17 +9,17 @@ class Sg90(Sensor):
   def __init__(self, arg):
     self.argnum = 1
 
-    self.gpio = arg[0]
+    self.gpio = int(arg[0])
 
 
-  def rotate(pwm, fr, to):
+  def rotate(self, pwm, fr, to):
     dire = 1
     if fr > to:
        dire = -1
     
     for degree in range(dire * fr, dire * to):
         dc = 2.5 + (12.0-2.5)/180*(dire * degree+90)
-        p.ChangeDutyCycle(dc)
+        pwm.ChangeDutyCycle(dc)
         time.sleep(0.03)
  
 
@@ -30,7 +30,7 @@ class Sg90(Sensor):
     p.start(0.0)
     time.sleep(1.0)
 
-    rotate(p, 90, -90)
+    self.rotate(p, 90, -90)
 
     #for degree in range(-90, 90):
     #    dc = 2.5 + (12.0-2.5)/180*(-degree+90)
@@ -39,7 +39,7 @@ class Sg90(Sensor):
 
     time.sleep(2.0)
 
-    rotate(p, -90, 90)
+    self.rotate(p, -90, 90)
     #for degree in range(-90, 90):
     #    dc = 2.5 + (12.0-2.5)/180*(degree+90)
     #    p.ChangeDutyCycle(dc)
